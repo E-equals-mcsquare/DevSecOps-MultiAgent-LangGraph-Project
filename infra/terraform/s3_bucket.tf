608b1ede72c519c09d13dbc276485058f45967bf
Souvik Majumder <36430328+E-equals-mcsquare@users.noreply.github.com>
2026-08-15 12:50:58 +0530

    demo: live recording take 2, real AWS dynamic credentials on HCP Terraform

--- infra/terraform/s3_bucket.tf
+++ infra/terraform/s3_bucket.tf
@@ -10,3 +10,5 @@ resource "aws_s3_bucket_public_access_block" "public_bucket" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+# live demo recording (take 2): dynamic AWS credentials now enabled on HCP Terraform
